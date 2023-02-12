4b230450528d060b76d4cc642dbef9ced46762a9
Aditya Kamble <adityakamble6101@gmail.com>
2023-02-12 22:16:51 +0530

    init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold font-inter">Hello world!</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.cjs
@@ -0,0 +1,14 @@
+const { m } = require("framer-motion");
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
